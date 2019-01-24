6fcc886f89013cafbebdbc4319bfc457c45a3a20
Chandrashekar Vankayalapati <chandrashekar.vankayalapati@gmail.com>
2019-01-24 06:56:38 +0530

    quick find

--- algos/src/main/java/QuickFindUnionFind.java
+++ algos/src/main/java/QuickFindUnionFind.java
@@ -14,14 +14,22 @@ public class QuickFindUnionFind {
         return id[p] == id[q];
     }
 
+    /**
+     * Takes quadratic time.
+     *
+     * @param p
+     * @param q
+     */
     public void union(int p, int q) {
 
         if (id[p] == id[q])
             return;
 
+        int pid = id[p];
+        int qid = id[q];
         for (int i = 0; i < id.length; i++) {
-            if (id[i] == id[p])
-                id[i] = id[q];
+            if (id[i] == pid)
+                id[i] = qid;
         }
     }
 }
